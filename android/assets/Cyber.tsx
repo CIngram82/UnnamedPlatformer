d5facc93c8a5e3ac808383c0d330b1d1ee8d0ade
Christopher Ingram <cingram82@gmail.com>
2017-10-25 18:29:07 -0400

    working on sci-fi map

---
+++ android/assets/Cyber.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="cyber_citadel_tiles" tilewidth="32" tileheight="32" tilecount="11" columns="1">
+ <image source="Sci-fi pixel tilesets/cyber_citadel/cyber_citadel/cyber_citadel_tiles.png" width="37" height="370"/>
+</tileset>
